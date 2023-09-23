e744ac5ad18bd0b7e30ef731912a8c5be120f383
xiangshiyin <piupiuyin@gmail.com>
2023-09-23 08:43:17 -0400

    more samples

---
+++ 2023-2024/2023-09-23/sample_1.py
@@ -0,0 +1,8 @@
+import turtle
+
+t = turtle.Turtle()
+t.pen(pencolor="red", fillcolor="white", pensize=30, speed=9)
+t.begin_fill()
+t.circle(90)
+t.end_fill()
+turtle.done()
---
+++ 2023-2024/2023-09-23/sample_3.py
@@ -0,0 +1,8 @@
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(100, 400, 100):
+    t.circle(i)
+
+turtle.done()
