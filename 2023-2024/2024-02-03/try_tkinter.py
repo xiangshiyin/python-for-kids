d88c0d210b4e12f5f4df79ae2e3a828d39354fda
xiangshiyin <piupiuyin@gmail.com>
2024-02-03 12:08:03 -0500

    tkinter example

---
+++ 2023-2024/2024-02-03/try_tkinter.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Tic Tac Toe")
+
+label = tk.Label(text="Hello World")
+label.pack()
+
+l2 = tk.Label(text="another hello")
+l2.pack()
+
+l3 = tk.Label(text="hello hello hello")
+l3.pack()
