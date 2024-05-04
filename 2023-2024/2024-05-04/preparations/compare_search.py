e0a94a29b845f7c96db986cfbcc34e0f19b45f8a
xiangshiyin <piupiuyin@gmail.com>
2024-05-03 23:44:31 -0400

    tested visual

--- 2023-2024/2024-05-04/preparations/compare_search.py
+++ 2023-2024/2024-05-04/preparations/compare_search.py
@@ -1,5 +1,6 @@
 import random
 
+
 def binary_search(arr, target):
     arr.sort()
     left = 0
@@ -27,26 +28,34 @@ def linear_search(arr, target):
             return i, steps
     return -1, steps
 
+
 # Compare the number of steps to find the target number in 100 random sorted list of integers of increasing lengths using linear search and binary search
 
+
 def compare_search():
-    for i in range(10, 100000, 100):
+    linear_search_steps = []
+    binary_search_steps = []
+    for i in range(10, 100000, 1000):
         arr = [random.randint(0, i) for i in range(i)]
         target = random.randint(0, i)
-        linear_search_steps = []
-        binary_search_steps = []
         linear_search_steps.append(linear_search(arr, target)[1])
         binary_search_steps.append(binary_search(arr, target)[1])
-    
-        # print(f"Length: {i}, Linear Search: {sum(linear_search_steps)/len(linear_search_steps)}, Binary Search: {sum(binary_search_steps)/len(binary_search_steps)}")
-    
+
+        print(
+            f"Length: {i}, Linear Search: {linear_search_steps[-1]}, Binary Search: {binary_search_steps[-1]}"
+        )
+
+    # print(len(list(range(10, 100000, 1000))))
+    # print(len(linear_search_steps))
+    # print(len(binary_search_steps))
+
     import matplotlib.pyplot as plt
 
-    plt.plot(range(10, 100000, 100), linear_search_steps, label='Linear Search')    
-    plt.plot(range(10, 100000, 100), binary_search_steps, label='Binary Search')
-    plt.xlabel('Length of list')
-    plt.ylabel('Number of steps')
-    plt.title('Linear Search vs Binary Search')
+    plt.plot(range(10, 100000, 1000), linear_search_steps, label="Linear Search")
+    plt.plot(range(10, 100000, 1000), binary_search_steps, label="Binary Search")
+    plt.xlabel("Length of list")
+    plt.ylabel("Number of steps")
+    plt.title("Linear Search vs Binary Search")
     plt.legend()
     plt.show()
 
